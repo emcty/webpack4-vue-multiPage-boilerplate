7902cb393ded290a7c79dd2f94f6358cdf4eb925
dl <dinglei@jianlc.com>
2018-11-23 18:21:17 +0800

    delete

--- src/assets/js/global.path.setting.js
+++
@@ -1,6 +0,0 @@
-;
-(function(global, factory) {
-    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() : typeof define === 'function' && define.amd ? define(factory) : global.GLOBAL_PATH = factory();
-})(this, function() {
-    'use strict';
-return {"URL_HTML":"","URL_HTML_WAP":"","URL_IMG_PUBLICPATH":"","URL_API":"http://10.103.27.92:8212","URL_WX_WAP":"","URL_WX_VERIFY":"","ID_WX_APP":"","ID_TD_APP":"418A817198F5451082CC0FD05F3419C6","URL_PUSH_DATA":"http://10.103.27.159:8080","KEY_PUSH_DATA":"TTFDTestAppAndroidIOSH5","COMPANY_NAME":"北京金未来金融信息服务有限公司","COMPANY_ICP":"京ICP备14056527号-5","SERVICE_Tel":"400-030-8585"}})
